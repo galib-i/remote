aabc0615bf01649a4118c9a21cc59ab36092600f
Galib Islam <217484167+galib-i@users.noreply.github.com>
2025-09-12 23:33:51 +0100

    Initial setup: basic Gin setup

---
+++ cmd/server/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"os/exec"
+	"runtime"
+
+	"remote/internal/router"
+)
+
+func openBrowser(url string) {
+	switch runtime.GOOS {
+	case "windows":
+		exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	}
+}
+
+func main() {
+	r := router.Init()
+
+	go openBrowser("http://localhost:8080")
+
+	r.Run(":8080")
+}
